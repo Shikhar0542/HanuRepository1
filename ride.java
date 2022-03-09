786bd6c7981e47965fde65d4564969c2c7a6bb8a
Shikhar0542 <101243244+Shikhar0542@users.noreply.github.com>
2022-03-09 15:05:28 +0530

    Add files via upload

---
+++ ride.java
@@ -0,0 +1,22 @@
+public class ride {
+	
+	public void display()
+	{
+		System.out.println("lets have a good enviroment");
+	}
+}
+class gone extends ride
+{
+	public void display()
+	{
+		System.out.println("Lets have the world beautiful");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		gone o1=new gone();
+		o1.display();
+
+	}
+
+}
