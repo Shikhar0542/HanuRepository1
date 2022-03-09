418b862b9a0483054ffe9eb45aa8ff73888d9c3c
Shikhar0542 <101243244+Shikhar0542@users.noreply.github.com>
2022-03-09 14:46:11 +0530

    Add files via upload
    Changes in the String1 file

--- string1.java
+++ string1.java
@@ -4,6 +4,7 @@ public static void main(String[] args)
 {
 String s1=new String("hanu");
 String s2=new String("hanu");
+Sting s3="Iam a pro player";
 if(s1.equals(s2))
 System.out.println("equal");
 if(s1==s2)
