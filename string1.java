79153d419dbb955a574d45d0e70d67b1adeb35fe
Shikhar0542 <101243244+Shikhar0542@users.noreply.github.com>
2022-03-09 14:27:55 +0530

    NewRepository1

---
+++ string1.java
@@ -0,0 +1,14 @@
+class string1
+{
+public static void main(String[] args)
+{
+String s1=new String("hanu");
+String s2=new String("hanu");
+if(s1.equals(s2))
+System.out.println("equal");
+if(s1==s2)
+System.out.println("equal");
+else
+System.out.println("not equal");
+}
+}
---
+++ switchkaro.java
@@ -0,0 +1,15 @@
+class switchkaro
+{
+public static void main(String args[])
+{
+int a=101;
+switch(a)
+{
+case 100:
+System.out.println("100 is inside");
+break;
+default:
+System.out.println("something there = " +a);
+}
+}
+}
